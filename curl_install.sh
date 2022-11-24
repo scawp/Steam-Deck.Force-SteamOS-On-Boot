ba198db45f73bcc4aeb178cefcda5ec6316f69ad
scawp <rob@scawp.com>
2022-11-24 20:26:43 +0100

    Initial Commit

---
+++ curl_install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#Steam Deck Force-SteamOS-On-Boot by scawp
+#License: DBAD: https://github.com/scawp/Steam-Deck.Force-SteamOS-On-Boot/blob/main/LICENSE.md
+#Source: https://github.com/scawp/Steam-Deck.Force-SteamOS-On-Boot
+# Use at own Risk!
+
+#curl -sSL https://raw.githubusercontent.com/scawp/Steam-Deck.Force-SteamOS-On-Boot/main/curl_install.sh | bash
+
+#stop running script if anything returns an error (non-zero exit )
+set -e
+
+repo_url="https://raw.githubusercontent.com/scawp/Steam-Deck.Force-SteamOS-On-Boot/main"
+
+tmp_dir="/tmp/scawp.SDFSOB.install"
+
+service_install_dir="/etc/systemd/system"
+
+device_name="$(uname --nodename)"
+
+if [ "$device_name" != "steamdeck" ]; then
+  zenity --question --width=400 \
+  --text="This code has been written specifically for the Steam Deck \
+  \nIt appears you are running on a different system/non-standard configuration. \
+  \nAre you sure you want to continue?"
+  if [ "$?" != 0 ]; then
+    #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
+    echo "bye then! xxx"
+    exit 1;
+  fi
+fi
+
+zenity --question --width=400 \
+  --text="Read $repo_url/README.md before proceeding. \
+\nDo you want to install the Force-SteamOS-On-Boot service?"
+if [ "$?" != 0 ]; then
+  #NOTE: This code will never be reached due to "set -e", the system will already exit for us but just incase keep this
+  echo "bye then! xxx"
+  exit 0;
+fi
+
+echo "Making tmp folder $tmp_dir"
+mkdir -p "$tmp_dir"
+
+echo "Downloading Required Files"
+curl -o "$tmp_dir/force-steamos-on-next-boot.service" "$repo_url/force-steamos-on-next-boot.service"
+
+echo "Copying $tmp_dir/force-steamos-on-next-boot.service to $service_install_dir/force-steamos-on-next-boot.service"
+sudo cp "$tmp_dir/force-steamos-on-next-boot.service" "$service_install_dir/force-steamos-on-next-boot.service"
+
+echo "Starting Service"
+sudo systemctl start force-steamos-on-next-boot.service
+
+echo "Done."
